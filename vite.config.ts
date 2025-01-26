4fecdb207affbda126a5d99ec46e12a6e6140b44
Nejc <nejc.mu@gmail.com>
2025-01-26 20:52:09 +0800

    chore: configure github actions and semantic-release

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,25 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { peerDependencies } from './package.json';
+
+const perDeps = Object.keys(peerDependencies);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: "./src/index.ts",
-      name: "wagmi-extended",
+      entry: './src/index.ts',
+      name: '@nejcm/wagmi-extended',
       fileName: (format) => `wagmi-extended.${format}.js`,
     },
     rollupOptions: {
       // Ensure external dependencies are not bundled into the library
-      external: ["react", "react-dom"],
+      external: perDeps,
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
